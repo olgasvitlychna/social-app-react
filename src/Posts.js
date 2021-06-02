c929bb00678c658e8abf731f419c5d5ee1e66abc
olgasvitlychna <olga.svitlychna9@gmail.com>
2021-06-02 12:53:57 +0200

    scroll1

--- src/Posts.js
+++ src/Posts.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './Posts.css'
 import axios from 'axios'
-
+import TimeAgo from 'timeago-react'
 // import timeAgo from '../utils/DateUtils';
 
 function Posts() {
     const [postsList, setPostsList] = useState([]);
     const [loading, setLoading] = useState(false);
     const [newPosts, setNewPosts] = useState([]);
+    const [lastPostDate, setLastPostDate] = useState('');
     const [endOfPage, setEndOfPage] = useState(false);
     useEffect(() => {
 
@@ -25,7 +26,8 @@ function Posts() {
             axiosConfig)
             .then((res) => {
 
-                setPostsList(res.data)
+                setPostsList(res.data);
+
 
             })
             .catch((err) => {
@@ -35,6 +37,10 @@ function Posts() {
     }, [])
 
     const postsElements = postsList.map((post) => {
+        // setLastPostDate(post.created_at)
+        // console.log(post)
+
+
 
 
         return (
@@ -60,17 +66,58 @@ function Posts() {
     });
 
     const scrollToEndOfPage = () => {
-        console.log('scroll')
+        setEndOfPage(true)
         
-    }
+        let axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+
+            }
+        };
+
+        axios.post(
+            'https://akademia108.pl/api/social-app/post/latest',
+            {},
+            axiosConfig)
+            .then((res) => {
+                console.log(res.data)
+                setNewPosts(res.data)
+
+            })
+            .catch((err) => {
+                console.log("AXIOS ERROR: ", err);
+            })
 
     
+}
+    const olderThanPosts = newPosts.map((post) => {
+        return(
+            <div className="postsContainer" key={post.id}>
+                <div className='usersInformation'>
+                    <div className="avatar">
+                        <img className="avatarImg" src={post.user.avatar_url} alt={post.user.username} />
+                    </div>
+                    <span className='userName'>{post.user.username}</span>
+                    <span className='postsData' >{post.timeAgo}</span>
+                </div>
+                <p className="post">{post.content}</p>
+                <div className='like'>
+
+                    <span> 123</span>
+                </div>
+
+            </div>
+        )
+    })
+
+
     window.onscroll = function () {
 
         if (document.documentElement.scrollHeight === (window.innerHeight + document.documentElement.scrollTop)) {
             scrollToEndOfPage()
             setLoading(true)
-
+            // setEndOfPage(true)
         }
     }
     return (
@@ -81,7 +128,9 @@ function Posts() {
             {loading &&
                 <div className="lds-facebook"><div></div><div></div><div></div></div>
             }
-            
+            {/* {endOfPage && */}
+                {olderThanPosts}
+            {/* } */}
 
         </div>
     )
